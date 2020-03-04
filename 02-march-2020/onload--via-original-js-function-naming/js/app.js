23f9234098881113aaf06416b2c08cf350c9b881
Roy A Vanegas <roy@thecodeeducators.com>
2020-03-04 12:15:03 -0500

    Add example of onload using legacy JS

---
+++ 02-march-2020/onload--via-original-js-function-naming/js/app.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    let x = 10, /* */
+        y = 20, //
+        z = 30;
+
+    let nfirstName = `ed`;
+    let nlastName = `bob`;
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log(`The entire name is ${nfirstName} ${nlastName}`);
+};
