44b06206ec7d7323f7228f35e8da366b196cbd7c
Roy A Vanegas <roy@thecodeeducators.com>
2020-03-04 12:16:03 -0500

    Add arrow function example for the onload event

---
+++ 02-march-2020/onload--via-arrow-function/js/app.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+    let x = 10, /* */
+        y = 20, //
+        z = 30; //
+    const MY_MAX_NUM = 30;
+
+    let nfirstName = `ed`;
+    let nlastName = `bob`;
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+    console.log(`The entire name is ${nfirstName} ${nlastName}`);
+
+    var firstName = 'ed';
+    var lastName = 'bob';
+
+    console.log('The entire name is ' + firstName + ' ' + lastName);
+
+    console.log(typeof MY_MAX_NUM);
+    console.log(typeof x);
+};
